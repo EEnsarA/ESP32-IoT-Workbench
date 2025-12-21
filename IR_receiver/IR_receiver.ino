e5e19a1cf4aee711b0de0207c97a12fa01ed1228
EEnsarA <efdal.atc@gmail.com>
2025-12-21 15:43:22 +0300

    .

--- IR_receiver/IR_receiver.ino
+++ IR_receiver/IR_receiver.ino
@@ -12,25 +12,18 @@ void setup() {
   
   IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK); 
 
-  Serial.println("Sistem Hazır! Kumandayı sensöre tut ve tuşlara bas...");
-
 }
 
 void loop() {
 
   if (IrReceiver.decode()) {
     
-    // Özet bilgiyi ekrana yaz
-    // Burada "Protocol=NEC Address=0x..." gibi şeyler yazar.
     IrReceiver.printIRResultShort(&Serial);
     
-    // Asıl işimize yarayan HEX kodunu ayrıca yazdıralım
     Serial.print("HEX Kodu: 0x");
     Serial.println(IrReceiver.decodedIRData.command, HEX);
     
     Serial.println("------------------------------------------------");
-
-    // Dinlemeye devam et (Bunu demezsen tek tuştan sonra donar)
     IrReceiver.resume(); 
   }
 
